67d3d99c2a6c02086e3ee5320e30a7e55341c3b7
albertsalles4 <albertsalles4@gmail.com>
2023-04-29 16:59:08 +0200

    Add server port and host as env variables and initialise with python main.py

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,4 @@ COPY app ./app
 
 RUN pip install -r requirements.txt
 
-CMD ["uvicorn", "app.main:app", "--host=0.0.0.0", "--port=8000"]
+CMD ["python", "app/main.py"]
--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.config import SERVER_HOST, SERVER_PORT, DEBUG
 from telegram_handler import telegram_webhook
 from twilio_handler import twilio_api_reply
 
@@ -8,3 +9,7 @@ app = FastAPI()
 # Include the routers for the Telegram webhook and Twilio API reply
 app.include_router(telegram_webhook)
 app.include_router(twilio_api_reply)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host=SERVER_HOST, port=SERVER_PORT, reload=DEBUG)

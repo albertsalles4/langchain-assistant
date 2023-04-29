7b77dc31b9b6e052fc5904c10a870ca6690b183d
albertsalles4 <albertsalles4@gmail.com>
2023-04-29 16:58:49 +0200

    Set all config with env variables

--- app/config.py
+++ app/config.py
@@ -3,6 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Server host and port
+SERVER_HOST = os.getenv('SERVER_HOST', '0.0.0.0')
+SERVER_PORT = int(os.getenv('SERVER_PORT', 8000))
+DEBUG = os.getenv('DEBUG', 'True') in ["True", "true", "1"]
+
 # Telegram Bot API token (default: None)
 TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN', None)
 
@@ -23,22 +28,22 @@ ZAPIER_NLA_API_KEY = os.getenv('ZAPIER_NLA_API_KEY', None)
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # Choose your model between gpt-3, gpt-3.5-turbo, gpt-4
-SELECTED_MODEL = 'gpt-4'
+SELECTED_MODEL = os.getenv('OPENAI_MODEL', 'gpt-4')
 
 # Temperature value for OpenAI language model
-TEMPERATURE_VALUE = float(0.8)
+TEMPERATURE_VALUE = float(os.getenv('TEMPERATURE_VALUE', 0.7))
 
 # DALL-E settings
-IMAGE_SIZE = "256x256"
+IMAGE_SIZE = os.getenv('DALL-E_IMAGE_SIZE', '256x256')
 
 # Chatbot name for generating responses
-BOT_NAME = 'Lago'
+BOT_NAME = os.getenv('BOT_NAME', 'Lago')
 
 # Use BabyAGI or not?
-BABYAGI = False
+BABYAGI = os.getenv('USE_BABYAGI', 'False') in ["True", "true", "1"]
 
 # Where to store conversation memories etc.
 HISTORY_DIR = './history'
 
 # TODO Numbers For testing purposes only, should be changed later
-MEMORYCONFIG = {'K_contextual':2, 'K_latest':4} 
+MEMORYCONFIG = {'K_contextual': 2, 'K_latest': 4}
